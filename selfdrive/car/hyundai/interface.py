7f7f9fc9aded187e9cf895911d3c87d5a1c6d666
durango-op <71580184+durango-op@users.noreply.github.com>
2020-10-04 19:10:24 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -73,14 +73,14 @@ class CarInterface(CarInterfaceBase):
       ret.steerLimitTimer = 2.5
       #lqr 조향 시작
       ret.lateralTuning.init('lqr')
-      ret.lateralTuning.lqr.scale = 1950
-      ret.lateralTuning.lqr.ki = 0.005
+      ret.lateralTuning.lqr.scale = 1600
+      ret.lateralTuning.lqr.ki = 0.01
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
-      ret.lateralTuning.lqr.k = [-100., 450.]
-      ret.lateralTuning.lqr.l = [0.22, 0.318]
-      ret.lateralTuning.lqr.dcGain = 0.00288
+      ret.lateralTuning.lqr.k = [-110., 451.]
+      ret.lateralTuning.lqr.l = [0.33, 0.318]
+      ret.lateralTuning.lqr.dcGain = 0.00225
       #lqr 조향 종료       
       
       
